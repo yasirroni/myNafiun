a54f67f39afc4822bf85ac03b9887edf06cc12be
yasirroni <muhammadyasirroni@gmail.com>
2020-07-04 06:59:06 +0700

    char counter

--- lib/dict2csv.py
+++ lib/dict2csv.py
@@ -1,6 +1,11 @@
-def dict2csv(path,dict_data):
+def dict2csv(path,dict_data,comma_handler=True):
 	with open(path,'w') as csv_out:
 		for key,value in dict_data.items():
+			if comma_handler==True:
+				if ',' in key:
+					key='"'+key+'"'
+				if ',' in str(value):
+					value='"'+str(value)+'"'
 			csv_out.write(key+','+str(value))
 			csv_out.write('\n')
 
@@ -9,7 +14,8 @@ if __name__=='__main__':
 	dict_data={
 		'Key':'Value',
 		'Name':'Test',
-		'Number':25
+		'Number':25,
+		'Job, Position':'Teacher, Homeroom Teacher'
 	}
 	
 	import os
--- lib/text_mining.py
+++ lib/text_mining.py
@@ -1,59 +1,85 @@
 import numpy as np
 import string
 import pandas as pd
-def text_mining(path,white_list=None,black_list=None,remove_punctuation=False,lower_case=False,encoding='utf-8'):
+def text_mining(path,white_list=None,black_list=None,lower_case=False,remove_punctuation=False,encoding='utf-8',char_counting=False):
 	'''Manual
 	white_list			: None or str containing white list
 	black_list			: None or str containing black list
-	remove_punctuation	: remove punctuation listed in string.punctuation and predefined black list
 	lower_case			: If True, convert text to lower_case
+	remove_punctuation	: remove punctuation listed in string.punctuation and predefined black list
 	encoding			: default is utf-8
+	char_counting		: If True, count char instead of text
 	'''
+	def _remove(input_string,black_list):
+		return(input_string.translate(str.maketrans('', '', black_list)))
+
+	def _lower_case(input_string):
+		return(input_string.lower())
+
+	def _dict(key,dict_keys):
+		if key in dict_keys:
+			return dict_keys[key]+1
+		else:
+			return 1
+
+	def _char_counting(txt_in,white_list=white_list,black_list=black_list,lower_case=lower_case):
+		dict_result={}
+		if white_list == None:
+			#black_list_method
+			for line in txt_in:
+				line=_remove(input_string=line,black_list=black_list)
+				if lower_case==True:
+					line=_lower_case(line)
+				for text in line.split():
+					for char in text:
+						dict_result[char]=_dict(char,dict_result)
+		else:
+			#white_list_method
+			for line in txt_in:
+				if lower_case==True:
+					line=_lower_case(line)
+				for text in line.split():
+					for char in text:
+						if char in white_list:
+							dict_result[char]=_dict(char,dict_result)
+		return dict_result
+
+	def _text_mining(txt_in,white_list=white_list,black_list=black_list,lower_case=lower_case):
+		dict_result={}
+		if white_list == None:
+			#black_list_method
+			for line in txt_in:
+				line=_remove(input_string=line,black_list=black_list)
+				if lower_case==True:
+					line=_lower_case(line)
+				for text in line.split():
+					dict_result[text]=_dict(text,dict_result)
+		else:		
+			#white_list_method
+			for line in txt_in:
+				if lower_case==True:
+					line=_lower_case(line)
+				for text in line.split():
+					text_white=''
+					for char in text:
+						if char in white_list:
+							text_white=text_white+char
+					dict_result[char]=_dict(char,dict_result)
+		return dict_result
+	
 	if black_list == None:
 		black_list = ''
 
 	if remove_punctuation == True:
 		punctuation=string.punctuation
 		black_list = black_list+punctuation
-
-	def _remove(input_string):
-		input_string=input_string.translate(str.maketrans('', '', black_list))
-		return(input_string)
-
-	def _lower_case(input_string):
-		input_string=input_string.lower()
-		return(input_string)
-
-	def _dict(text,dict_result):
-		if text in dict_result:
-			dict_result[text]=dict_result[text]+1
-		else:
-			dict_result[text]=1
-
-	dict_result={}
+		
 	with open(path,encoding=encoding) as txt_in:
-		if white_list == None:
-			#black_list_method
-			for line in txt_in:
-				line=_remove(line)
-				if lower_case==True:
-					line=_lower_case(line)
-				for text in line.split():
-					_dict(text,dict_result)
-		else:		
-			#white_list_method
-			for line in txt_in:
-				if lower_case==True:
-					line=_lower_case(line)
-				text=''
-				for char in line:
-					if char == ' ':
-						_dict(text,dict_result)
-						text=''
-					if char in white_list:
-						text=text+char
-				_dict(text,dict_result)	
-					
+		if char_counting == True:
+			dict_result=_char_counting(txt_in,white_list=white_list,black_list=black_list,lower_case=lower_case)
+		else:
+			dict_result=_text_mining(txt_in,white_list=white_list,black_list=black_list,lower_case=lower_case)
+	
 	#cleaning
 	if '' in dict_result: del dict_result['']
 	return dict_result
@@ -67,6 +93,8 @@ if __name__=='__main__':
 	result_raw=text_mining(path,remove_punctuation=True,black_list=black_list)
 	with open(os.path.join(save_path),'w') as txt_out:
 		for key,value in result_raw.items():
+			if ',' in key:
+				key='"'+key+'"'
 			txt_out.write(key+','+str(value))
 			txt_out.write('\n')
 	print(result_raw)
@@ -76,15 +104,39 @@ if __name__=='__main__':
 	result_lower_case=text_mining(path,remove_punctuation=True,lower_case=True,black_list=black_list)
 	with open(os.path.join(save_path),'w') as txt_out:
 		for key,value in result_lower_case.items():
+			if ',' in key:
+				key='"'+key+'"'
 			txt_out.write(key+','+str(value))
 			txt_out.write('\n')
 	print(result_lower_case)
 
-	white_list=string.ascii_letters+string.digits
+	white_list=string.ascii_letters+string.digits+'_'
 	save_path=os.path.join(os.getcwd(),'data\\[RESULT]_text_minning_white_list_methode.csv')
 	result_lower_case=text_mining(path,remove_punctuation=True,lower_case=True,white_list=white_list)
 	with open(os.path.join(save_path),'w') as txt_out:
 		for key,value in result_lower_case.items():
+			if ',' in key:
+				key='"'+key+'"'
+			txt_out.write(key+','+str(value))
+			txt_out.write('\n')
+	print(result_lower_case)
+
+	save_path=os.path.join(os.getcwd(),'data\\[RESULT]_text_minning_char_counting.csv')
+	result_lower_case=text_mining(path,char_counting=True)
+	with open(os.path.join(save_path),'w') as txt_out:
+		for key,value in result_lower_case.items():
+			if ',' in key:
+				key='"'+key+'"'
+			txt_out.write(key+','+str(value))
+			txt_out.write('\n')
+	print(result_lower_case)
+
+	save_path=os.path.join(os.getcwd(),'data\\[RESULT]_text_minning_char_counting_lower_case.csv')
+	result_lower_case=text_mining(path,lower_case=True,char_counting=True)
+	with open(os.path.join(save_path),'w') as txt_out:
+		for key,value in result_lower_case.items():
+			if ',' in key:
+				key='"'+key+'"'
 			txt_out.write(key+','+str(value))
 			txt_out.write('\n')
 	print(result_lower_case)
